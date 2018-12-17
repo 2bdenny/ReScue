fd4c74249514206c85874c5fd0aaf574a7191e2e
denny <denny.syj@hotmail.com>
2018-12-17 21:20:13 +0800

    add java extractor

---
+++ test/scripts/extractor/java.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import sys
+from utils.base import getRegFromProject, storeRegs
+
+def searchJavaFile(dir):
+    regs = getRegFromProject(dir, 'grep -P "(Pattern\.compile|\.replace)\(\\\".*?[^\\\\\\\\]\\\"" -rno --include \\*.java', r'(.*\.java):(\d+):(Pattern\.compile|\.replace)\s*\(\s*\"(.*?)\"$', raw_group = 4, lang = 'java')
+    storeRegs(regs)
+
+# searchJavaFile(sys.argv[1])
